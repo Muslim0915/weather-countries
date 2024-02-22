0c91615a7c17295f248c9756682059bdb4a8a7e8
Muslim0915 <muslimpro0915@gmail.com>
2024-02-22 14:16:26 +0500

    adding loader

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,41 @@
-let currentLocation = 'TASHKENT';
-let API = `https://api.weatherapi.com/v1/current.json?key=43cbfe0275934e6b96e54230242202&q=Tashkent&aqi=yes`;
-const weatherContainer = document.querySelector('.weather__container');
-const weatherLocationTitle = document.querySelector('.location');
-const searchButton = document.querySelector('.fa-search');
+let loader = document.querySelector('.lds-dual-ring');
+const weatherRepository = {
+    API_URL: 'https://api.weatherapi.com/v1/current.json?key=43cbfe0275934e6b96e54230242202&aqi=yes&q=',
 
-const updateWeather = async () => {
+    async fetch(currentLocation) {
+        showLoader()
+        const response = await fetch(`${this.API_URL}${currentLocation}`);
+        return await response.json();
+    }
+}
+
+const LOCATION_DEFAULT = 'TASHKENT';
+
+const searchButton = document.querySelector('.fa-search');
+const weatherWrapper =  document.querySelector('.container')
+const weatherContainer = weatherWrapper.querySelector('.weather__container');
+const weatherLocationTitle = weatherWrapper.querySelector('.location');
+const weatherLocaltime = weatherWrapper.querySelector('.weather__localtime');
+
+const updateWeather = async (currentLocation) => {
     try {
-        const response = await fetch(API);
-        const data = await response.json();
+        const data = await weatherRepository.fetch(currentLocation);
         setWeatherStates(data);
-        console.log(data);
     } catch (error) {
         console.log(error);
         weatherContainer.innerHTML = 'You probably entered the cities incorrectly or have bad internet.';
-        weatherLocationTitle.innerHTML = ''
+        weatherLocationTitle.innerHTML = '';
+        weatherLocaltime.innerHTML = '';
+    }
+    finally {
+        hideLoader()
     }
 };
 
 const setWeatherStates = (data) => {
-    const weatherLocaltime = document.querySelector('.weather__localtime');
-    weatherLocationTitle.innerHTML = `${data.location.name}, ${data.location.country}`
-    weatherContainer.innerHTML = `
+    weatherLocationTitle.innerHTML = `${data.location.name}, ${data.location.country}`;
+    weatherContainer.innerHTML = ''; // Очищаем контейнер перед обновлением
+    weatherContainer.innerHTML += `
         <div class="weather__icon">
             <img src="${data.current.condition.icon}" alt="icon">
         </div>
@@ -38,27 +53,38 @@ const setWeatherStates = (data) => {
     weatherLocaltime.innerHTML = data.location.localtime;
 };
 
-const saveData = () => {
+const saveData = (currentLocation) => {
     localStorage.setItem('currentLocation', currentLocation);
 };
 
 const loadAndSetWeather = async () => {
-    currentLocation = localStorage.getItem('currentLocation') || currentLocation;
-    API = `https://api.weatherapi.com/v1/current.json?key=43cbfe0275934e6b96e54230242202&q=${currentLocation}&aqi=yes`;
-    await updateWeather();
+    const currentLocation = localStorage.getItem('currentLocation') || LOCATION_DEFAULT || location;
+    await updateWeather(currentLocation);
 };
 
 window.onload = loadAndSetWeather;
+
 async function searchLocation() {
-    currentLocation = document.querySelector('.search-input').value;
+    let currentLocation = document.querySelector('.search-input').value;
+    saveData(currentLocation);
+    await loadAndSetWeather(currentLocation);
     document.querySelector('.search-input').value = '';
-    saveData();
-    await loadAndSetWeather();
 }
-window.addEventListener('keydown', async (event)=>{
-    if(event.key === 'Enter'){
+
+window.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter') {
         await searchLocation();
     }
-})
-searchButton.addEventListener('click',() => searchLocation());
+});
 
+searchButton.addEventListener('click', () => searchLocation());
+
+
+
+function showLoader() {
+    loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+    loader.classList.add('hidden');
+}
